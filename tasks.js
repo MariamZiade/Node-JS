19b3aa80064b7c884620073e75997836f9dcb0ec
MariamZiade <mariam.ziade@gmail.com>
2021-03-29 23:20:39 +0300

    extended hello

--- tasks.js
+++ tasks.js
@@ -1,55 +1,51 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
 function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`Welcome to ${name}'s application!`);
+  console.log("--------------------");
 }
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text.trim() === 'quit' || text.trim() === 'exit') {
+  text = text.replace("\n", " ").trim();
+  var arrayText = text.split(" ");
+  console.log(arrayText);
+
+  if (text.trim() === "quit" || text.trim() === "exit") {
     quit();
+  } else if (text.trim() === "help") {
+    help();
+  } else if (arrayText[0] === "hello") {
+    hello(arrayText[1]);
+  } else {
+    unknownCommand(text);
   }
-
-  else if (text.trim() === 'help'){
-    help(); 
-  }
-
-  else if (text === 'hello\n') {
-  hello();
 }
-else {
-  unknownCommand(text);
-}
-}
-
 
 /**
  * prints "unknown command"
@@ -59,18 +55,18 @@ else {
  * @returns {void}
  */
 function unknownCommand(c) {
-  console.log('unknown command: "' + c.trim() + '"')
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello() {
-  console.log('hello!')
-}
+function hello(text) {
+
+    console.log("hello" +" "+ text + "!")
+  }
 
 
 /**
@@ -79,16 +75,28 @@ function hello() {
  * @returns {void}
  */
 function quit() {
-  console.log('Quitting now, goodbye!')
+  console.log("Quitting now, goodbye!");
   process.exit();
 }
 
 // The following help command lists all the possible commands
 
 function help() {
-  console.log('All the possible commands: \n' ,'\n','hello\n', 'quit\n', 'help\n', 'list\n', 'add\n', 'remove\n', 'update\n', 'check\n', 'uncheck\n')
+  console.log(
+    "All the possible commands: \n",
+    "\n",
+    "hello\n",
+    "quit\n",
+    "help\n",
+    "list\n",
+    "add\n",
+    "remove\n",
+    "update\n",
+    "check\n",
+    "uncheck\n"
+  );
   process.exit();
 }
 
 // The following line starts the application
-startApp("Mariam Ziadé")
+startApp("Mariam Ziadé");
